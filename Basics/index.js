375c1c0b776a13cbda7d9379321087fb56008e16
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-09-18 19:53:46 +0500

    Create index.js

---
+++ Basics/index.js
@@ -0,0 +1,14 @@
+let name = 'Waleed';
+console.log(name);
+
+//rules
+//Cannot be a reserved word
+//Should be  meaningful
+//cannot start with a number(1name)
+//Cannot contain a space or hypine(-)
+//Are Case-sensitive
+
+let firstName = 'waleed';
+let lastName = 'tahir';
+
+console.log(firstName +''+ lastName);
